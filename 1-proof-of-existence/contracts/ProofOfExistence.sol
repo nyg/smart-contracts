89acb541259e79e59157fe8736e6ca0d56d50f48
nyg <nyg@users.noreply.github.com>
2021-05-14 15:17:38 +0200

    Add comments and use custom error

--- 1-proof-of-existence/contracts/ProofOfExistence.sol
+++ 1-proof-of-existence/contracts/ProofOfExistence.sol
@@ -1,26 +1,43 @@
 // SPDX-License-Identifier: ISC
 pragma solidity 0.8.4;
 
+/// @notice This contract allows senders to notarize documents. A hash of the
+/// document will be stored in the contract's state.
+///
+/// Note that the content of the document is stored in the transaction, it is
+/// therefore public and influences the cost of the transaction to notarize it.
 contract ProofOfExistence {
 
-    // state: by default, each value of a corresponding key is set to false
-    // when we store a proof, we set the value to true
-    mapping (bytes32 => bool) private proofs;
+    /// @notice Error thrown if a sender tries to notarize a document that has
+    /// already been so.
+    error DocumentAlreadyNotarized();
 
-    // notarize the given document, i.e. store the proof in the contract's state
+    /// @notice Indicates if a proof has been stored in the contract's state.
+    mapping(bytes32 => bool) private proofs;
+
+    /// @notice Notarize the given document, i.e. compute the document's proof
+    /// (SHA-256 hash) and store the proof in the contract's state.
     function notarize(string calldata document) external {
         bytes32 proof = proofFor(document);
+        if (proofs[proof]) {
+            revert DocumentAlreadyNotarized();
+        }
+
         proofs[proof] = true;
     }
 
-    // check if the given document has already been notarized
-    function checkDocument(string memory document) public view returns (bool) {
-        bytes32 proof = proofFor(document);
-        return proofs[proof];
+    /// @notice Check if the given document has already been notarized, i.e. if
+    /// its proof already exists in the contract's state.
+    function wasNotarized(string calldata document)
+        external
+        view
+        returns (bool)
+    {
+        return proofs[proofFor(document)];
     }
 
-    // helper function to compute proof of given document
-    function proofFor(string memory document) private pure returns (bytes32) {
+    /// @notice Helper function to compute proof of a given document.
+    function proofFor(string calldata document) private pure returns (bytes32) {
         return sha256(abi.encodePacked(document));
     }
 }
