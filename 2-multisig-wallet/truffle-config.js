ff42b7895489ffd1c16f99aed4cb05f735944b83
nyg <nyg@users.noreply.github.com>
2021-05-14 18:30:32 +0200

    Remove symlink for truffle-config

--- 2-multisig-wallet/truffle-config.js
+++ 2-multisig-wallet/truffle-config.js
@@ -1,1 +1,28 @@
-../truffle-config-default.js
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const fs = require('fs')
+
+const [mnemonic, infuraKey] = fs.readFileSync('../.secret').toString().split('\n')
+const infuraURL = `https://ropsten.infura.io/v3/${infuraKey}`
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
+
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 3,
+      gas: 5500000
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: '0.8.4'
+    }
+  },
+}
