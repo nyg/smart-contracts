35eeb076d05a5d2974f89d6d2a8e6a20c8776db7
nyg <nyg@users.noreply.github.com>
2021-05-14 17:09:22 +0200

    Add unit test and emit event when document is notarized

--- 1-proof-of-existence/contracts/ProofOfExistence.sol
+++ 1-proof-of-existence/contracts/ProofOfExistence.sol
@@ -8,15 +8,19 @@ pragma solidity 0.8.4;
 /// therefore public and influences the cost of the transaction to notarize it.
 contract ProofOfExistence {
 
-    /// @notice Error thrown if a sender tries to notarize a document that has
-    /// already been so.
+    /// @notice Error thrown if a sender tries to notarize an already notarized
+    /// document.
     error DocumentAlreadyNotarized();
+
+    /// @notice Event emitted when a document has been successfully notarized.
+    event DocumentNotarized(bytes32 indexed proof);
 
     /// @notice Indicates if a proof has been stored in the contract's state.
     mapping(bytes32 => bool) private proofs;
 
+
     /// @notice Notarize the given document, i.e. compute the document's proof
-    /// (SHA-256 hash) and store the proof in the contract's state.
+    /// (Keccak-256 hash) and store the proof in the contract's state.
     function notarize(string calldata document) external {
         bytes32 proof = proofFor(document);
         if (proofs[proof]) {
@@ -24,20 +28,17 @@ contract ProofOfExistence {
         }
 
         proofs[proof] = true;
+        emit DocumentNotarized(proof);
     }
 
     /// @notice Check if the given document has already been notarized, i.e. if
     /// its proof already exists in the contract's state.
-    function wasNotarized(string calldata document)
-        external
-        view
-        returns (bool)
-    {
+    function wasNotarized(string calldata document) external view returns (bool) {
         return proofs[proofFor(document)];
     }
 
     /// @notice Helper function to compute proof of a given document.
     function proofFor(string calldata document) private pure returns (bytes32) {
-        return sha256(abi.encodePacked(document));
+        return keccak256(abi.encodePacked(document));
     }
 }
---
+++ 1-proof-of-existence/test/proof_of_existence.test.js
@@ -0,0 +1,31 @@
+const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers')
+const ProofOfExistence = artifacts.require('ProofOfExistence')
+
+
+contract('ProofOfExistence', accounts => {
+
+  const document = 'Lorem ipsum'
+
+  let instance
+  beforeEach(async () => {
+    instance = await ProofOfExistence.new()
+  })
+
+  it('should notarize a given document', async () => {
+
+    assert.isFalse(await instance.wasNotarized(document))
+
+    expectEvent(
+      await instance.notarize(document),
+      'DocumentNotarized',
+      { proof: web3.utils.soliditySha3(document) })
+
+    assert.isTrue(await instance.wasNotarized(document))
+  })
+
+  it('should not notarize an already document', async () => {
+    await instance.notarize(document)
+    await expectRevert.unspecified(
+      instance.notarize(document))
+  })
+})
