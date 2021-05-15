f06d97ab61ad54e4703b9612e304f33b540d0ee4
nyg <nyg@users.noreply.github.com>
2021-05-15 21:14:54 +0200

    Add receive and fallback functions
    As well as:
    - make set function payable
    - emit Deposit event when funds are received

--- 2-multisig-wallet/contracts/SimpleStorage.sol
+++ 2-multisig-wallet/contracts/SimpleStorage.sol
@@ -2,11 +2,38 @@
 pragma solidity 0.8.4;
 
 contract SimpleStorage {
-    uint256 public storedData;
+
+    /// @notice Simple storage variable to hold some data.
+    uint256 public data;
+
+    /// @notice Caller of the set function.
     address public caller;
 
-    function set(uint256 x) public {
+    /// @notice A deposit of Ether has been made to the contract.
+    event Deposit(address indexed sender, uint256 value);
+
+    /// @notice Allow the contract to receive Ether if calldata is empty.
+    receive() external payable {
+        emitEventIfFundsReceived();
+    }
+
+    /// @notice Same as receive() but called if calldata is invalid.
+    fallback() external payable {
+        emitEventIfFundsReceived();
+    }
+
+    /// @notice Set data storage variable to new value and caller to
+    /// msg.sender. The function is payable in case it needs to receive Ether.
+    function set(uint256 _data) external payable {
         caller = msg.sender;
-        storedData = x;
+        data = _data;
+        emitEventIfFundsReceived();
+    }
+
+    /// @notice Emit an event if funds were received.
+    function emitEventIfFundsReceived() private {
+        if (msg.value > 0) {
+            emit Deposit(msg.sender, msg.value);
+        }
     }
 }
