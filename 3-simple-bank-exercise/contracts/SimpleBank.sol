e78faa1b5b23ef333c5715af760490bca6cd6ae8
nyg <nyg@users.noreply.github.com>
2021-06-06 14:45:25 +0200

    Remove fallback function and revert if withdrawal fails

--- 3-simple-bank-exercise/contracts/SimpleBank.sol
+++ 3-simple-bank-exercise/contracts/SimpleBank.sol
@@ -50,15 +50,6 @@ contract SimpleBank {
         owner = msg.sender;
     }
 
-    // Fallback function - Called if other functions don't match call or
-    // sent ether without data
-    // Typically, called when invalid data is sent
-    // Added so ether sent to this contract is reverted if the contract fails
-    // otherwise, the sender's money is transferred to contract
-    //fallback() external payable {
-    //    revert();
-    //}
-
     /// @notice Returns the balance of the sender's account.
     function getBalance() external view returns (uint256) {
         return balances[msg.sender];
@@ -96,17 +87,15 @@ contract SimpleBank {
         senderMustBeEnrolled()
         returns (uint256)
     {
-        if (balances[msg.sender] >= amount) {
-            balances[msg.sender] -= amount;
+        require(balances[msg.sender] >= amount, "Unsufficient funds");
 
-            (bool sent, ) = msg.sender.call{value: amount}("");
-            require(sent, "Withdrawal failed");
+        balances[msg.sender] -= amount;
 
-            uint256 newBalance = balances[msg.sender];
-            emit LogWithdrawalMade(msg.sender, amount, newBalance);
-            return newBalance;
-        }
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Withdrawal failed");
 
-        return 0;
+        uint256 newBalance = balances[msg.sender];
+        emit LogWithdrawalMade(msg.sender, amount, newBalance);
+        return newBalance;
     }
 }
