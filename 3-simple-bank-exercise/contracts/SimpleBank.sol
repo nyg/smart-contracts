e8b9b700cc3127c07db74b9594cfabd86cb147f6
nyg <nyg@users.noreply.github.com>
2021-06-06 13:59:56 +0200

    Complete functions part

--- 3-simple-bank-exercise/contracts/SimpleBank.sol
+++ 3-simple-bank-exercise/contracts/SimpleBank.sol
@@ -2,15 +2,14 @@
 pragma solidity 0.8.4;
 
 contract SimpleBank {
-
     /*
      * State variables
      */
 
-    /// @notice Account balances of the bank's clients.
+    /// @notice Account balances of the clients of the bank.
     mapping(address => uint256) private balances;
 
-    /// @notice The bank's clients.
+    /// @notice The clients of the bank.
     mapping(address => bool) public enrolled;
 
     /// @notice The owner of the bank.
@@ -20,22 +19,35 @@ contract SimpleBank {
      * Events
      */
 
-    /// @notice A client has enrolled in the bank.
+    /// @notice A client has enrolled with the bank.
     event LogEnrolled(address indexed account);
 
     /// @notice A deposit was made.
     event LogDepositMade(address indexed account, uint256 amount);
 
     /// @notice A withdrawal was made.
-    event LogWithdrawalMade(address indexed account, uint256 amount, uint256 newBalance);
+    event LogWithdrawalMade(
+        address indexed account,
+        uint256 amount,
+        uint256 newBalance
+    );
+
+    /*
+     * Function modifiers
+     */
+
+    modifier senderMustBeEnrolled() {
+        require(enrolled[msg.sender], "Sender must be enrolled");
+        _;
+    }
 
     /*
      * Functions
      */
 
-    /* Use the appropriate global variable to get the sender of the transaction */
+    /// @notice Sets the creator of the contract as the owner of the bank.
     constructor() {
-        /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -43,43 +55,58 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    fallback() external payable {
-        revert();
+    //fallback() external payable {
+    //    revert();
+    //}
+
+    /// @notice Returns the balance of the sender's account.
+    function getBalance() external view returns (uint256) {
+        return balances[msg.sender];
     }
 
-    /// @notice Get balance
-    /// @return The balance of the user
-    // A SPECIAL KEYWORD prevents function from editing state variables;
-    // allows function to run locally/off blockchain
-    function getBalance() public returns (uint256) {
-        /* Get the balance of the sender of this transaction */
+    /// @notice Enrolls the sender with the bank.
+    /// @return The client's enrolled status.
+    function enroll() external returns (bool) {
+        if (!enrolled[msg.sender]) {
+            enrolled[msg.sender] = true;
+            emit LogEnrolled(msg.sender);
+        }
+
+        return true;
     }
 
-    /// @notice Enroll a customer with the bank
-    /// @return The users enrolled status
-    // Emit the appropriate event
-    function enroll() public returns (bool) {}
-
-    /// @notice Deposit ether into bank
-    /// @return The balance of the user after the deposit is made
-    // Add the appropriate keyword so that this function can receive ether
-    // Use the appropriate global variables to get the transaction sender and value
-    // Emit the appropriate event
-    // Users should be enrolled before they can make deposits
-    function deposit() public returns (uint256) {
-        /* Add the amount to the user's balance, call the event associated with a deposit,
-          then return the balance of the user */
+    /// @notice Deposits Ether into the bank.
+    /// @return The client's account balance after the deposit was made.
+    function deposit()
+        external
+        payable
+        senderMustBeEnrolled()
+        returns (uint256)
+    {
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balances[msg.sender];
     }
 
-    /// @notice Withdraw ether from bank
-    /// @dev This does not return any excess ether sent to it
-    /// @param withdrawAmount amount you want to withdraw
-    /// @return The balance remaining for the user
-    // Emit the appropriate event
-    function withdraw(uint256 withdrawAmount) public returns (uint256) {
-        /* If the sender's balance is at least the amount they want to withdraw,
-           Subtract the amount from the sender's balance, and try to send that amount of ether
-           to the user attempting to withdraw.
-           return the user's balance.*/
+    /// @notice Withdraws Ether from the bank.
+    /// @param amount The amount of Ether to be withdrawn.
+    /// @return The client's account balance after the withdrawal was made.
+    function withdraw(uint256 amount)
+        external
+        senderMustBeEnrolled()
+        returns (uint256)
+    {
+        if (balances[msg.sender] >= amount) {
+            balances[msg.sender] -= amount;
+
+            (bool sent, ) = msg.sender.call{value: amount}("");
+            require(sent, "Withdrawal failed");
+
+            uint256 newBalance = balances[msg.sender];
+            emit LogWithdrawalMade(msg.sender, amount, newBalance);
+            return newBalance;
+        }
+
+        return 0;
     }
 }
