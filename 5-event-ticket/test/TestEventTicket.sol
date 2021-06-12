540040d91be0c51d8b7ece6c89fcee80cebe343e
nyg <nyg@users.noreply.github.com>
2021-06-13 00:38:28 +0200

    Remove solidity test file

--- 5-event-ticket/test/TestEventTicket.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.8.5;
-
-import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
-import "../contracts/EventTickets.sol";
-import "../contracts/EventTicketsV2.sol";
-
-contract TestEventTicket {}
