117ef4f8c582a71e0569601bfb3675562638b535
nyg <nyg@users.noreply.github.com>
2021-08-08 15:34:02 +0200

    Add update script to update all dependencies

---
+++ npm-dep-update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+echo "Installing npm-check-updates…"
+npm -g i npm-check-updates
+
+for dir in */ ; do
+	cd "$dir"
+	if [ -f package.json ] ; then
+		echo "\n\n==========================="
+		echo "== Updating dependencies of ${dir}…"
+		echo "==========================="
+		ncu -u && npm install
+	fi
+	cd ..
+done
+
+echo "Done."
