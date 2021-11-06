b1492f255293f44959c7ee3cd45fb00323bf4510
nyg <nyg@users.noreply.github.com>
2021-11-06 22:47:00 +0100

    Minor changes

--- 8-oz-transparent-proxy-pattern/migrations/1-deploy-contract.js
+++ 8-oz-transparent-proxy-pattern/migrations/1-deploy-contract.js
@@ -4,9 +4,9 @@ const BoxV1 = artifacts.require('BoxV1')
 
 module.exports = async function (deployer) {
 
-  // Validate that the implementation is upgrade safe.
-  // Deploy a proxy admin for your project.
-  // Deploy the implementation contract.
-  // Create and initialize the proxy contract.
+  // 1. Validate that the implementation is upgrade safe.
+  // 2. Deploy a proxy admin for your project.
+  // 3. Deploy the implementation contract.
+  // 4. Create and initialize the proxy contract.
   await deployProxy(BoxV1, [42], { deployer, initializer: 'initializeV1' })
 }
