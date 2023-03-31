2bad0f8cb3763a22d61408bfb700d7b24cdde945
nyg <nyg@users.noreply.github.com>
2023-03-31 22:50:55 +0200

    Minor changes

--- 1-proof-of-existence/hardhat.config.js
+++ 1-proof-of-existence/hardhat.config.js
@@ -1,8 +1,5 @@
 require('@nomicfoundation/hardhat-toolbox')
 
-// const fs = require('fs')
-// const [mnemonic, infuraApiKey] = fs.readFileSync('../.secret').toString().split('\n')
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
    solidity: '0.8.18'
